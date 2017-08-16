e64d21643cdb7f7a2ea48931c5c4980589318414
loldlm1 <loldlm1@gmail.com>
2017-08-16 11:16:27 -0400

    added IO_ERROR

--- src/CryptoNoteCore/DataBaseErrors.h
+++ src/CryptoNoteCore/DataBaseErrors.h
@@ -46,6 +46,7 @@ public:
       case static_cast<int>(DataBaseErrorCodes::NOT_INITIALIZED) : return "Object was not initialized";
       case static_cast<int>(DataBaseErrorCodes::ALREADY_INITIALIZED) : return "Object has been already initialized";
       case static_cast<int>(DataBaseErrorCodes::INTERNAL_ERROR) : return "Internal error";
+      case static_cast<int>(DataBaseErrorCodes::IO_ERROR) : return "IO error";
       default: return "Unknown error";
     }
   }
